88f8325e04bac6518859104268bd60adcdee4480
usamahbass <basalamahusamah7@gmail.com>
2022-12-22 16:08:49 +0700

    change: sort selection in icon

--- src/icons/SortSelectionSelectedIcon.jsx
+++ src/icons/SortSelectionSelectedIcon.jsx
@@ -3,9 +3,7 @@ const SortSelectionSelectedIcon = () => (
     width={15}
     height={15}
     viewBox="0 0 15 15"
-    data-cy="sort-selection-icon"
     fill="none"
-    data-cy="sort-selection-selected"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
